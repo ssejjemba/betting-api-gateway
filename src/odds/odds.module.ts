d3c7078e9bf8ebf779c9d1cfac7f6550bf8f100a
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-28 03:12:30 +0300

    (Odds): Add module entry for odds service

--- src/odds/odds.module.ts
+++ src/odds/odds.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ODDS_SERVICE_NAME, ODDS_PACKAGE_NAME } from './odds.pb';
 import { OddsController } from './odds.controller';
 
 @Module({
-  controllers: [OddsController]
+  imports: [
+    ClientsModule.register([
+      {
+        name: ODDS_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: ODDS_PACKAGE_NAME,
+          protoPath: 'node_modules/betting-proto/proto/odds.proto',
+        },
+      },
+    ]),
+  ],
+  controllers: [OddsController],
 })
-export class OddsModule {}
+export class OrderModule {}
