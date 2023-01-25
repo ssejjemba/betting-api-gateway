088072d7dd0c4cb1f108857fa2ccc8f95d60690b
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-25 23:52:33 +0300

    Add JWT secret as env variable

--- config/config.ts
+++ config/config.ts
@@ -4,4 +4,5 @@ export const ENV_VARS = {
   grpcAuthPort: process.env.GRPC_AUTH_PORT,
   grpcOddsPort: process.env.GRPC_ODDS_PORT,
   httpServerPort: process.env.HTTP_SERVER_PORT,
+  jwtSecret: process.env.JWT_TOKEN_SECRET,
 };
