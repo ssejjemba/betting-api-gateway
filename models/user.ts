edc39c31523c1325640d885f7d5f3a16aa8a7f3d
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-01-25 23:02:10 +0300

    Add model data for the user

---
+++ models/user.ts
@@ -0,0 +1,50 @@
+export enum Role {
+  USER = 0,
+  ADMIN = 1,
+  SUPER_ADMIN = 2,
+}
+
+export type User = {
+  username: string;
+  email: string;
+  password: string;
+  role: Role;
+};
+
+export type UserResponse = {
+  username: string;
+  email: string;
+  role: Role;
+};
+
+export type SignUpRequest = User;
+
+export type SignUpResponse = {
+  code: number;
+  error: string;
+  user: UserResponse | null;
+  token: string;
+};
+
+export type SignInRequest = {
+  email: string;
+  password: string;
+};
+
+export type SignInResponse = {
+  code: number;
+  error: string;
+  user: UserResponse | null;
+  token: string;
+};
+
+export type VerifyTokenRequest = {
+  token: string;
+};
+
+export type VerifyTokenResponse = {
+  code: number;
+  error: string;
+  email: string;
+  role: Role;
+};
