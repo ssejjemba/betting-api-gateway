d1df41c31b57b2d15eb1178ad8a83eeb7d9b0267
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-02-07 22:17:56 +0300

    Update import to come from models Odds type

--- dtos/odd.ts
+++ dtos/odd.ts
@@ -1,8 +1,23 @@
-import { Odd } from "../models/odds";
+import { Odd } from "../models/requests";
 
 // will transform odd snake case to pascal case
 export class OddDTO {
+  id: number;
+  league: string;
+  homeTeam: string;
+  awayTeam: string;
+  gameDate: string;
+  homeTeamWinOdd: number;
+  drawOdd: number;
+  awayTeamWinOdd: number;
   constructor(data: Odd) {
-    this;
+    this.id = data.id;
+    this.league = data.league;
+    this.homeTeam = data.home_team;
+    this.awayTeam = data.away_team;
+    this.gameDate = data.game_date;
+    this.homeTeamWinOdd = data.home_team_win_odd;
+    this.drawOdd = data.draw_odd;
+    this.awayTeamWinOdd = data.away_team_win_odd;
   }
 }
