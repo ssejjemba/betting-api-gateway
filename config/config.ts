ca99e00bb999ea4358f8f15b9a1ea8ad58041803
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-02-07 21:53:48 +0300

    Update configurations with host names

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,8 @@ require("dotenv").config();
 export const ENV_VARS = {
   grpcAuthPort: process.env.GRPC_AUTH_PORT,
   grpcOddsPort: process.env.GRPC_ODDS_PORT,
+  grpcAuthHost: process.env.GRPC_AUTH_HOST,
+  grpcOddsHost: process.env.GRPC_ODDS_HOST,
   httpServerPort: process.env.HTTP_SERVER_PORT,
   jwtSecret: process.env.JWT_TOKEN_SECRET,
   jwtExpireDuration: process.env.JWT_EXP_DURATION,
