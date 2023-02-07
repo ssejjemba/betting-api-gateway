f89324d535081a7571bc3da8f945a1c438b15b8b
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-02-08 01:40:32 +0300

    Add bash scripts to generate protoc files

---
+++ compile-odds-protoc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+OUT_DIR="./generated"
+TS_OUT_DIR="./generated"
+IN_DIR="node_modules/betting-proto/proto"
+PROTOC="$(npm bin)/grpc_tools_node_protoc"
+PROTOC_GEN_TS_PATH="$(npm bin)/protoc-gen-ts"
+PROTOC_GEN_GRPC_PATH="$(npm bin)/grpc_tools_node_protoc_plugin"
+
+$PROTOC \
+    -I="./node_modules/betting-proto/proto" \
+    --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH \
+    --plugin=protoc-gen-grpc=$PROTOC_GEN_GRPC_PATH \
+    --js_out=import_style=commonjs:$OUT_DIR \
+    --grpc_out=grpc_js:$OUT_DIR \
+    --ts_out=service=grpc-node,mode=grpc-js:$TS_OUT_DIR \
+    "$IN_DIR"/odds.proto
