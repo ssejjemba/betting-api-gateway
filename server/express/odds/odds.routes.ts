5f2922fede87280a170b541638708c02047900be
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-02-09 22:40:43 +0300

    Apply authetication middleware to odds router

--- server/express/odds/odds.routes.ts
+++ server/express/odds/odds.routes.ts
@@ -1,16 +1,37 @@
 import { AppRoutes } from "../../../models/server";
 import { Router, Request, Response, NextFunction } from "express";
 import { IOddsService } from "../../../models/services";
+import { JWTAuthetication } from "../../../middleware/jwt/jwt.middleware";
 
 export class OddRoutes implements AppRoutes {
   private readonly router = Router();
-  constructor(public oddsService: IOddsService) {}
+  private middleware: JWTAuthetication;
+
+  constructor(public oddsService: IOddsService) {
+    this.middleware = new JWTAuthetication();
+  }
 
   makeRoutes() {
-    this.router.post("/api/create", this.create.bind(this));
-    this.router.post("/api/read", this.read.bind(this));
-    this.router.post("/api/update", this.update.bind(this));
-    this.router.post("/api/delete", this.delete.bind(this));
+    this.router.post(
+      "/odds/create",
+      this.middleware.isAutheticated,
+      this.create.bind(this)
+    );
+    this.router.post(
+      "/odds/read",
+      this.middleware.isAutheticated,
+      this.read.bind(this)
+    );
+    this.router.post(
+      "/odds/update",
+      this.middleware.isAdminAuthorized,
+      this.update.bind(this)
+    );
+    this.router.post(
+      "/odds/delete",
+      this.middleware.isAdminAuthorized,
+      this.delete.bind(this)
+    );
   }
 
   create(req: Request, res: Response, next: NextFunction) {
